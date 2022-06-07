a51b9dba0bbdf6a07a9848e602c8f7c12c9a958d
mathewmelvinng <54765658+mathewmelvinng@users.noreply.github.com>
2022-06-07 21:41:07 +1000

    created Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
+        // Storing player's input
         float input = Input.GetAxisRaw("Horizontal");
-        print(input);
+
+        //Moving player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+     
     }
 }
